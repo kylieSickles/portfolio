79a12bd501fb0b1a71bf6b42fcb8f9864e43ce0b
kylieSickles <kylie.e.sickles@gmail.com>
2024-04-10 22:08:35 -0700

    Edited padding and added flower bullet points

--- public/js/main.js
+++ public/js/main.js
@@ -85,54 +85,59 @@ window.onscroll = function() {
 
 
 
-// Image Slider
-// Select all slides
-const slides = document.querySelectorAll(".slide");
+// // Image Slider
+// // Select all slides
+// const slides = document.querySelectorAll(".slide");
 
-// loop through slides and set each slides translateX
-slides.forEach((slide, indx) => {
-  slide.style.transform = `translateX(${indx * 100}%)`;
-});
+// // loop through slides and set each slides translateX
+// slides.forEach((slide, indx) => {
+//   slide.style.transform = `translateX(${indx * 100}%)`;
+// });
 
-// select next slide button
-const nextSlide = document.querySelector(".btn-next");
+// // select next slide button
+// const nextSlide = document.querySelector(".btn-next");
 
-// current slide counter
-let curSlide = 0;
-// maximum number of slides
-let maxSlide = slides.length - 1;
+// // current slide counter
+// let curSlide = 0;
+// // maximum number of slides
+// let maxSlide = slides.length - 1;
 
-// add event listener and navigation functionality
-nextSlide.addEventListener("click", function () {
-  // check if current slide is the last and reset current slide
-  if (curSlide === maxSlide) {
-    curSlide = 0;
-  } else {
-    curSlide++;
-  }
+// // add event listener and navigation functionality
+// nextSlide.addEventListener("click", function () {
+//   // check if current slide is the last and reset current slide
+//   if (curSlide === maxSlide) {
+//     curSlide = 0;
+//   } else {
+//     curSlide++;
+//   }
 
-  //   move slide by -100%
-  slides.forEach((slide, indx) => {
-    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
-  });
-});
+//   //   move slide by -100%
+//   slides.forEach((slide, indx) => {
+//     slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+//   });
+// });
 
-// select next slide button
-const prevSlide = document.querySelector(".btn-prev");
+// // select next slide button
+// const prevSlide = document.querySelector(".btn-prev");
 
-// add event listener and navigation functionality
-prevSlide.addEventListener("click", function () {
-  // check if current slide is the first and reset current slide to last
-  if (curSlide === 0) {
-    curSlide = maxSlide;
-  } else {
-    curSlide--;
-  }
+// // add event listener and navigation functionality
+// prevSlide.addEventListener("click", function () {
+//   // check if current slide is the first and reset current slide to last
+//   if (curSlide === 0) {
+//     curSlide = maxSlide;
+//   } else {
+//     curSlide--;
+//   }
 
-  //   move slide by 100%
-  slides.forEach((slide, indx) => {
-    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
-  });
-});
+//   //   move slide by 100%
+//   slides.forEach((slide, indx) => {
+//     slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+//   });
+// });
+
+var projectHeight = document.getElementById("koug-title").style.height;
+console.log(projectHeight);
+console.log("haiiiii");
+document.querySelector('.cream-slide').style.height = projectHeight;
 
 
